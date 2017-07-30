1dc996e744d84770d66a8ee76c4fbebf00946669
Artem Sarkisov <artem.sarkisov@jetbrains.com>
2017-07-30 13:26:52 +0300

    Add interface.rb

---
+++ interface.rb
@@ -0,0 +1,21 @@
+require_relative 'dealer'
+require_relative 'player'
+require_relative 'deck'
+
+class Interface
+
+  def initialize
+    puts "Enter your name"
+    name           = gets.chomp
+    @player        = Player.new(name)
+    @dealer        = Dealer.new
+    @pack_of_cards = Deck.new
+  end
+
+  def take_cards
+    2.times do
+      @player.cards << @pack_of_cards.deck.delete_at(rand(@pack_of_cards.deck.length))
+      @dealer.cards << @pack_of_cards.deck.delete_at(rand(@pack_of_cards.deck.length))
+    end
+  end
+end
