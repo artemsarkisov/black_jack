b34e1da9f91922e714340a4c46dc40b6723f5268
Artem Sarkisov <artem.sarkisov@jetbrains.com>
2017-07-30 13:26:44 +0300

    Add deck.rb

---
+++ deck.rb
@@ -0,0 +1,23 @@
+require_relative 'card'
+
+class Deck
+
+  attr_reader :deck
+
+  def initialize
+    @ranks = %w(6 7 8 9 10 J Q K A)
+    @suits = %w(Spades Hearts Diamonds Clubs)
+    @deck ||= []
+
+    @suits.each do |suit|
+      @ranks.size.times do |i|
+        @deck << Card.new(@ranks[i], suit)
+      end
+    end
+    @deck.shuffle!
+  end
+end
+
+# pack_of_cards = Deck.new
+
+# pack_of_cards.deck.card_info
