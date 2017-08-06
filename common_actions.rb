372f4689369e974046eac93a244ce84ef01db76b
Artem Sarkisov <artem.sarkisov@jetbrains.com>
2017-08-06 23:20:39 +0300

    cut excessive methods from common_actions module

--- common_actions.rb
+++ common_actions.rb
@@ -1,51 +1,40 @@
 module CommonActions
-  def self.included(base)
-    base.extend(ClassMethods)
-    base.send :include, InstanceMethods
+  attr_reader :sum
+
+  @@bank = 0
+
+  def take_card
+    raise 'You already have two cards' unless @cards.length <= 2
+    @pack_of_cards                 ||= Deck.new
+    @cards[@pack_of_cards.deck[0]] = @pack_of_cards.deck[0].value
+    @pack_of_cards.deck.delete_at(0)
+    sum_of_cards
+  rescue => e
+    puts e.message
+    return
   end
 
-  module ClassMethods
-    ;
+  def sum_of_cards
+    @sum = @cards.values.inject(:+)
+    @sum -= 10 if @cards.value?(11) && @sum > 21
   end
 
-  module InstanceMethods
-    attr_reader :sum
+  def place_bet(rate = 10)
+    @account.amount -= rate
+    @@bank          += rate
+  end
 
+  def clear_bank
     @@bank = 0
+  end
 
-    def take_card
-      raise 'You already have two cards' unless @cards.length <= 2
-      @pack_of_cards                 ||= Deck.new
-      @cards[@pack_of_cards.deck[0]] = @pack_of_cards.deck[0].value
-      @pack_of_cards.deck.delete_at(0)
-      sum_of_cards
-    rescue => e
-      puts e.message
-      return
-    end
-
-    def sum_of_cards
-      @sum = @cards.values.inject(:+)
-      @sum -= 10 if @cards.value?(11) && @sum > 21
-    end
-
-    def place_bet(rate = 10)
-      @account.amount -= rate
-      @@bank          += rate
-    end
-
-    def clear_bank
-      @@bank = 0
-    end
-
-    def winner_gain
-      @account.amount += @@bank
-      clear_bank
-    end
+  def winner_gain
+    @account.amount += @@bank
+    clear_bank
+  end
 
 
-    def show_cards(block)
-      @cards.each_key { |card| block.call(card) }
-    end
+  def show_cards(block)
+    @cards.each_key { |card| block.call(card) }
   end
 end
